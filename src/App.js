785aaf680b1492166ea540b64a509df3e357b1cc
“saxgutierrez” <diegofdog@gmail.com>
2018-08-03 19:13:30 -0500

    proyecto1 react solucionado mirar después mejora con redux

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button,Input,span,img,Container,Row,Col } from 'reactstrap';
 import './App.css';
 
- class App extends React.Component {
+class App extends React.Component {
   constructor(){
     super();
       this.state ={ 
@@ -50,16 +50,16 @@ import './App.css';
 
                   }
 
-      this.toggle1 = this.toggle1.bind(this);
-      this.toggle2 = this.toggle2.bind(this);
+                  this.Ascendente = this.Ascendente.bind(this);
+                  this.Descendente = this.Descendente.bind(this);  
   }
 
-  toggle1(event){
+  Ascendente(event){
     //hace el cambio de colores en los botones negando la misma variable
     this.setState({changeBottom: !this.state.changeBottom});
-    //como ordenas un arreglo de hash según el valor mayor del parámetro votes
+    
     let oldData = this.state.data;
-
+    //ordenar de mayor a menor
     let newData = oldData.sort((a,b)=>{
                                           if (a.votes > b.votes){
                                             return 1;
@@ -70,18 +70,15 @@ import './App.css';
                                             }
                                             // a must be equal to b
                                             return 0;
-
                                       });
-
-      
   }
 
-  toggle2(event){
+  Descendente(event){
     //hace el cambio de colores en los botones negando la misma variable
     this.setState({changeBottom: !this.state.changeBottom});
-    //como ordenas un arreglo de hash según el menor valor del parámetro votes
+    
     let oldData = this.state.data;
-
+    //ordenar de menor a mayor
     let newData = oldData.sort((firts,second)=>{
                                           if (firts.votes < second.votes){
                                             return 1;
@@ -90,53 +87,83 @@ import './App.css';
                                           if (firts.votes < second.votes) {
                                               return -1;
                                             }
-                                            // a must be equal to b
+                                            // first must be equal to second
                                             return 0;
-
                                       });
-
-     
-
   }
 
+  add(id,votes,event){
+     // console.log(id,votes); //impresión de los parametros id y votes
+        let oldData = this.state.data;
+         //iteracion y actualización de un parametro en particular, referente a un id específico
+        let newData = oldData.map(data=>{
 
-  //importansímo el id para hacer referencia a determinado elemento del json por default
-   handleChange(id,votes,event){
-    console.log(id,votes);//devuelve el id y el valor de votos
-    //guardo en una variable local el estado inicial y
-    //de allí para adelante esta trabaja como si fuera el mismo this.state.data que es valor por default
-    let oldData = this.state.data;
-    
-    //iteracion y actualización de un parametro en particular, referente a un id específico
-    let newData = oldData.map(data=>{
-
-                                        if(data.id===id)
-                                        //guarda el cambio del mismo input con lo que estaba cargado desde el json
+          if(data.id===id)
+            //guarda el cambio del mismo input con lo que estaba cargado desde el json
+              {
+                data.votes= data.votes+1
+                
+               return data
+              }
+          return data
+        })
+        //ordenar de mayor a menor
+        let newData1 = newData.sort((firts,second)=>{
+                                          if (firts.votes < second.votes)
                                           {
-                                            data.votes = event.target.votes  
-                                            
-                                           return data
+                                            return 1;
                                           }
-                                        return data
-                                    })  
 
-    
+                                          if (firts.votes < second.votes)
+                                          {
+                                            return -1;
+                                          }
+                                          // first must be equal to second
+                                            return 0;
+                                      });
 
-    this.setState({data:newData});
-   console.log(this.state.data);
+      this.setState({data:newData1});
+      // console.log(this.state.data);//impresión de data verificando el cambio 
+  }
 
+  subtract(id,votes,event){
+    // console.log(id,votes); //impresión de los parametros id y votes
+        let oldData = this.state.data;
+         //iteracion y actualización de un parametro en particular, referente a un id específico
+        let newData = oldData.map(data=>{
 
-     //como ordenas un arreglo de hash según el valor mayor del parámetro votes
-    //falta hacer que cambio de orden según se vaya cambiando de valor manualmente, es decir
-    //que se ordene de mayor a menor, es decir un cambio de posiciones dentro del arreglo de hash
+        if(data.id===id)
+          //guarda el cambio del mismo input con lo que estaba cargado desde el json
+            {
+              if(data.votes>0) // solo permite restar para números mayores a cero
+                {
+                  data.votes= data.votes-1
+                  return data
+                }
+              return data
+            }
+        return data    
+        })
 
-    //guarda y se muestra esta información
-   
-    //necesito actualizar el arreglo original con el nuevo atributo del newData
-    // this.setState({data:})
-    
+        //ordenar de mayor a menor
+        let newData1 = newData.sort((firts,second)=>{
+                                          if (firts.votes < second.votes)
+                                          {
+                                            return 1;
+                                          }
 
-    }
+                                          if (firts.votes < second.votes)
+                                          {
+                                            return -1;
+                                          }
+                                            // first must be equal to second
+                                            return 0;
+                                      });
+
+      this.setState({data:newData1});
+      // console.log(this.state.data);//impresión de data verificando el cambio
+  }
+
   render() {
     
     return (
@@ -150,44 +177,52 @@ import './App.css';
             <span className="pr-1">Orden:</span>
 
             <Button className={this.state.changeBottom === true? "btn btn-outline-primary" : ""} 
-             color="primary" size="lg" onClick={this.toggle1}>Ascendente</Button>{' '}
+             color="primary" size="lg" onClick={this.Ascendente}>Ascendente</Button>{' '}
 
             <Button className={this.state.changeBottom !== true? "btn btn-outline-primary" : ""}
-             color="primary" size="lg" onClick={this.toggle2}>Descendente</Button>{' '}
+             color="primary" size="lg" onClick={this.Descendente}>Descendente</Button>{' '}
 
           </Col>
 
           {this.state.data.map((data,index)=>
 
-              <Col  md={{ size: 8, offset: 2 }} className="mt-3 row">
+              <Col md={{ size: 8, offset: 2 }} className="mt-3 row">
 
-                <Col  md="4" className="">
-                  <img className="imagew" key={index} src={data.post_image_url}></img>
+                <Col  md="4" className="d-flex align-items-center">
+                  <img className="imagew" key={index}
+                   src={data.post_image_url}></img>
                 </Col>
 
-                <Col  md="2" className="">
-                  <div className="align-middle" >
-                    <Input type="number" min="0" key={index}
-                    onChange={this.handleChange.bind(this,data.id,data.votes)}
-                     value={data.votes} />
+                <Col md="2" className="d-flex align-items-center">
+                  <div className="text-center">
+
+                    <a color="primary" size="sm" 
+                    onClick={this.add.bind(this,data.id,data.votes)}>
+                    <i class="colorfa fas fa-caret-down fa-2x fa-rotate-180"></i></a>{' '}
+                    
+                    <Col sm={{ size: 10, offset: 1 }} className="">
+                      <Input type="number" min="0" key={index} className="text-center"
+                       value={data.votes} />
+                    </Col>
+
+                    <a color="primary" size="sm" 
+                    onClick={this.subtract.bind(this,data.id,data.votes)}>
+                    <i class="colorfa fas fa-caret-down fa-2x"></i></a>{' '}
+                    
                   </div>
                 </Col>
 
                 <Col  md="6" className="">
                   <p><a target="_blank" key={index} href={data.url}>{data.title}</a></p>
-                <p key={index} >{data.description}</p>
-                <p className="colortext">Escrito por: <span><img className="rounded-circle imagew1" key={index} src={data.writer_avatar_url}></img></span></p>
+                  <p key={index} >{data.description}</p>
+                  <p className="colortext">Escrito por: <span><img className="rounded-circle imagew1" key={index} src={data.writer_avatar_url}></img></span></p>
                 </Col>
 
               </Col>
-          
           )}
 
-
         </Row>
-      </Container>
-
-      
+      </Container> 
     );
   }
 }
